521f26f7d6616756555440eb3eee9bda0b3fb759
Kori Roys <kori@koriroys.com>
2012-10-23 20:24:00 -0500

    Remove extra conditional statement
    if the first conditional is met, an error will be raised and
    execution will stop there anyways, so no need for the else
    statement.

--- lib/my_struct.rb
+++ lib/my_struct.rb
@@ -6,13 +6,14 @@ class MyStruct
   def self.new(*args)
     if args.empty?
       raise ArgumentError, "wrong number of arguments (0 for 1+)"
-    else
-      args.each do |arg|
-        if arg.class != Symbol
-          raise TypeError
-        end
+    end
+
+    args.each do |arg|
+      if arg.class != Symbol
+        raise TypeError
       end
     end
+
     Class.new
   end
 end
