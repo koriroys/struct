d40688f04bf7dfbc52ea4c3c42d7b9b348654724
Kori Roys <kori@koriroys.com>
2012-10-23 20:47:32 -0500

    Increasing readability of each block to if.all?

--- lib/my_struct.rb
+++ lib/my_struct.rb
@@ -5,7 +5,7 @@
 class MyStruct
   def self.new(*args)
     raise ArgumentError, "wrong number of arguments (0 for 1+)" if args.empty?
-    args.each { |arg| raise TypeError unless arg.kind_of?(Symbol) }
+    raise TypeError unless args.all? { |arg| arg.kind_of?(Symbol) }
     Class.new
   end
 end
