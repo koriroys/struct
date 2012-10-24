94c263c7f2ae85fdc6e91c11cfcb932f4305d5fd
Kori Roys <kori@koriroys.com>
2012-10-23 20:37:19 -0500

    Change if statement to a positive unless statement

--- lib/my_struct.rb
+++ lib/my_struct.rb
@@ -5,9 +5,7 @@
 class MyStruct
   def self.new(*args)
     raise ArgumentError, "wrong number of arguments (0 for 1+)" if args.empty?
-
-    args.each { |arg| raise TypeError if arg.class != Symbol }
-
+    args.each { |arg| raise TypeError unless arg.class == Symbol }
     Class.new
   end
 end
