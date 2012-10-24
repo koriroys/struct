d5255b93e4abe838e6c10db9c7037d5f10ea590d
Kori Roys <kori@koriroys.com>
2012-10-23 20:02:47 -0500

    yay breaking brains

--- lib/my_struct.rb
+++ lib/my_struct.rb
@@ -1,6 +1,18 @@
-MyStruct = Struct
+# MyStruct = Struct
 
 # delete the above and create your own:
-# class MyStruct
-#   ...
-# end
+
+class MyStruct
+  def self.new(*args)
+    if args.empty?
+      raise ArgumentError, "wrong number of arguments (0 for 1+)"
+    else
+      args.each do |arg|
+        if arg.class != Symbol
+          raise TypeError
+        end
+      end
+      return Class.new
+    end
+  end
+end
